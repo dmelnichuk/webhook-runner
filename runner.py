fee31653e01e8073cd822f919465a4c94758e5fd
Dmitry Melnichuk <dmitry.melnichuk@nobitlost.com>
2019-06-05 13:21:57 +1000

    Improve logging.

--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
 import hmac
 import json
+import logging
+from threading import Thread
 
 from bottle import request, response, run
 from invoke.context import Context
@@ -8,27 +10,41 @@ from config import app
 from tasks import deploy
 
 
+logger = logging.Logger(name=__name__)
+
+
 @app.route('/webhook/', method='POST')
 def webhook():
+    logger.info('Delivery is started.')
     body = request._get_body_string()
 
     # check signature
-    signature = request.headers.get('X-Hub-Signature')
+    # set defaults to prevent giving out 500
+    signature = request.headers.get('X-Hub-Signature', '=')
     sha, signature = signature.split('=')
-    secret = str.encode(app.config.get('github.secret'))
+    secret = str.encode(app.config.get('github.secret', ''))
     digest = hmac.new(secret, body, digestmod='sha1').hexdigest()
     if hmac.compare_digest(digest, signature):
         # check branch or tag
         name = app.config.get('github.name', 'master')
         payload = json.loads(body.decode('utf-8'))
-
+        logger.info('Delivery ID={}'.format(
+            request.headers.get('X-GitHub-Delivery', 'Unknown'))
+        )
         # either 'refs/heads/<name>' or 'refs/tags/<name>'
-        if payload['ref'].split('/')[-1] == name:
+        pushed_name = payload['ref'].split('/')[-1]
+        logger.info('Name of a branch or a tag being pushed: {}'.format(
+            pushed_name
+        ))
+        if pushed_name == name:
             ctx = Context()
             ctx.config.update(app.config)
-            deploy(ctx)
+            Thread(target=deploy, args=(ctx, )).start()
+        else:
+            logger.info('Skipping delivery.')
     else:
-        response.status_code = 401
+        response.status = 401
+        logger.error('Delivery was not authorized.')
 
     return json.dumps({})
 
--- tasks.py
+++ tasks.py
@@ -1,42 +1,107 @@
+import logging
 import os
 
 from invoke import task
+from invoke.exceptions import Failure
+
+
+logger = logging.Logger(name=__name__)
+
+
+def run_and_log(ctx: 'Context', cmd: str, warn: bool = True) -> bool:
+    """
+    Log and run command, then log and process its output.
+
+    :param ctx: Invoke context,
+    :param cmd: the command,
+    :param warn: (optional) Invoke-compatible parameter. If you want to
+     continue running commands after the current command being failed, set
+     this parameter to True (default). If you want to stop the execution
+     of all the next commands with the `Failure` exception, set it to False,
+    :return: True if command was executed normally (has zero return code),
+     False otherwise.
+    """
+    logger.info('Running command:\n{}'.format(cmd))
+
+    result = ctx.run(cmd, hide='both', echo=False, warn=False)
+
+    if result.stdout:
+        logger.info(result.stdout)
+    if result.stderr:
+        logger.error(result.stderr)
+
+    if result.exited == 0:
+        return True
+    else:
+        logger.error('The last command failed!')
+
+    if not warn:
+        # generic exception, because logging was done already
+        raise Failure(result)
+
+    return False
 
 
 @task
 def deploy(ctx):
+    """ Deploy task. """
     name = ctx.config.get('github.name', 'master')
     base = os.path.expanduser(ctx.config.get('deploy.base', '~/www'))
     src = ctx.config.get('deploy.src', '.')
 
     if not os.path.isdir(os.path.join(base, src)):
         # create source directory
-        ctx.run('mkdir -p {}'.format(os.path.join(base, src)))
+        run_and_log(
+            ctx,
+            'mkdir -p {}'.format(os.path.join(base, src)),
+            warn=False,
+        )
 
     if not os.path.isdir(os.path.join(base, src, '.git')):
         # clone repository
         repo_url = ctx.config['github.url']
         with ctx.cd(base):
-            ctx.run('git clone "{}" {}'.format(repo_url, src))
+            run_and_log(
+                ctx,
+                'git clone "{}" {}'.format(repo_url, src),
+                warn=False,
+            )
 
     with ctx.cd(os.path.join(base, src)):
-        ctx.run('git fetch')
-        ctx.run('git checkout {}'.format(name))
+        run_and_log(ctx, 'git fetch', warn=False)
+        run_and_log(ctx, 'git checkout {}'.format(name), warn=False)
 
     if not os.path.isdir(os.path.join(base, 'env')):
         # TODO: automate it
-        print('Please create virtual environment and set up services!')
+        logger.error(
+            'Please create virtual environment and set up services!'
+        )
 
     else:
         with ctx.cd(base):
-            ctx.run((
-                'source env/bin/activate &&'
-                ' pip install --upgrade pip &&'
-                ' pip install -r requirements.txt &&'
-                ' ObeDog/manage.py migrate &&'
-                ' ObeDog/manage.py collectstatic --noinput'
-            ))
+            # can't back up now, warn=True
 
-            ctx.run('sudo systemctl restart gunicorn.service')
-            ctx.run('sudo systemctl restart celery-worker.service')
-            ctx.run('sudo systemctl restart celery-beat.service')
+            # issue Django commands
+            run_and_log(
+                ctx,
+                'source env/bin/activate && pip install --upgrade pip'
+            )
+            run_and_log(
+                ctx,
+                'source env/bin/activate && pip install -r requirements.txt'
+            )
+            run_and_log(
+                ctx,
+                'source env/bin/activate && ObeDog/manage.py migrate'
+            )
+            run_and_log(
+                ctx,
+                'source env/bin/activate && ObeDog/manage.py collectstatic --noinput'
+            )
+
+            # restart services
+            run_and_log(ctx, 'sudo systemctl restart gunicorn.service')
+            run_and_log(ctx, 'sudo systemctl restart celery-worker.service')
+            run_and_log(ctx, 'sudo systemctl restart celery-beat.service')
+
+    logger.info('Delivery is completed normally.')
